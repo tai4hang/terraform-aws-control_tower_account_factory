0e4a7a3467a4f2507dad93e3ddb2ab525f2546a4
Gary Chan <gary.chan@qualicom.com>
2024-08-15 12:09:28 -0400

    update pipeline

--- sources/aft-customizations-common/templates/customizations_pipeline/codepipeline.tf
+++ sources/aft-customizations-common/templates/customizations_pipeline/codepipeline.tf
@@ -51,6 +51,51 @@ resource "aws_codepipeline" "aft_codecommit_customizations_codepipeline" {
         PollForSourceChanges = false
       }
     }
+    
+    action {
+      name             = "aft-account-vpc"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeCommit"
+      version          = "1"
+      output_artifacts = ["source-aft-account-customizations-vpc"]
+
+      configuration = {
+        RepositoryName       = "tai4hang/learn-terraform-aft-account-customizations-vpc"
+        BranchName           = "main"
+        PollForSourceChanges = false
+      }
+    }
+    
+    action {
+      name             = "aft-account-iam"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeCommit"
+      version          = "1"
+      output_artifacts = ["source-aft-account-customizations-iam"]
+
+      configuration = {
+        RepositoryName       = "tai4hang/learn-terraform-aft-account-customizations-iam"
+        BranchName           = "main"
+        PollForSourceChanges = false
+      }
+    }
+    
+    action {
+      name             = "aft-account-sg"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeCommit"
+      version          = "1"
+      output_artifacts = ["source-aft-account-customizations-sg"]
+
+      configuration = {
+        RepositoryName       = "tai4hang/learn-terraform-aft-account-customizations-sg"
+        BranchName           = "main"
+        PollForSourceChanges = false
+      }
+    }
   }
 
   ##############################################################
@@ -83,7 +128,7 @@ resource "aws_codepipeline" "aft_codecommit_customizations_codepipeline" {
   # Apply-AFT-Account-Customizations
   ##############################################################
   stage {
-    name = "Account-Customizations"
+    name = "Account-Setup"
 
     action {
       name            = "Apply"
@@ -95,6 +140,87 @@ resource "aws_codepipeline" "aft_codecommit_customizations_codepipeline" {
       run_order       = "2"
       configuration = {
         ProjectName = var.aft_account_customizations_terraform_codebuild_name
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "VENDED_ACCOUNT_ID",
+            value = var.account_id,
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+
+  ##############################################################
+  # Apply-AFT-Account-VPC
+  ##############################################################
+  stage {
+    name = "Account-VPC"
+
+    action {
+      name            = "Apply"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source-aft-account-customizations-vpc"]
+      version         = "1"
+      run_order       = "2"
+      configuration = {
+        ProjectName = "aft-account-customizations-vpc-terraform"
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "VENDED_ACCOUNT_ID",
+            value = var.account_id,
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+
+  ##############################################################
+  # Apply-AFT-Account-IAM
+  ##############################################################
+  stage {
+    name = "Account-IAM"
+
+    action {
+      name            = "Apply"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source-aft-account-customizations-iam"]
+      version         = "1"
+      run_order       = "2"
+      configuration = {
+        ProjectName = "aft-account-customizations-iam-terraform"
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "VENDED_ACCOUNT_ID",
+            value = var.account_id,
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+
+  ##############################################################
+  # Apply-AFT-SG
+  ##############################################################
+  stage {
+    name = "Account-SG"
+
+    action {
+      name            = "Apply"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source-aft-account-customizations-sg"]
+      version         = "1"
+      run_order       = "2"
+      configuration = {
+        ProjectName = "aft-account-customizations-sg-terraform"
         EnvironmentVariables = jsonencode([
           {
             name  = "VENDED_ACCOUNT_ID",
@@ -161,6 +287,57 @@ resource "aws_codepipeline" "aft_codestar_customizations_codepipeline" {
         OutputArtifactFormat = "CODE_ZIP"
       }
     }
+    
+    action {
+      name             = "aft-account-customizations-vpc"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source-aft-account-customizations-vpc"]
+
+      configuration = {
+        ConnectionArn        = data.aws_ssm_parameter.codestar_connection_arn.value
+        FullRepositoryId     = "tai4hang/learn-terraform-aft-account-customizations-vpc"
+        BranchName           = "main"
+        DetectChanges        = false
+        OutputArtifactFormat = "CODE_ZIP"
+      }
+    }
+    
+    action {
+      name             = "aft-account-customizations-iam"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source-aft-account-customizations-iam"]
+
+      configuration = {
+        ConnectionArn        = data.aws_ssm_parameter.codestar_connection_arn.value
+        FullRepositoryId     = "tai4hang/learn-terraform-aft-account-customizations-iam"
+        BranchName           = "main"
+        DetectChanges        = false
+        OutputArtifactFormat = "CODE_ZIP"
+      }
+    }
+    
+    action {
+      name             = "aft-account-customizations-sg"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source-aft-account-customizations-sg"]
+
+      configuration = {
+        ConnectionArn        = data.aws_ssm_parameter.codestar_connection_arn.value
+        FullRepositoryId     = "tai4hang/learn-terraform-aft-account-customizations-sg"
+        BranchName           = "main"
+        DetectChanges        = false
+        OutputArtifactFormat = "CODE_ZIP"
+      }
+    }
   }
 
   ##############################################################
@@ -207,6 +384,84 @@ resource "aws_codepipeline" "aft_codestar_customizations_codepipeline" {
       run_order       = "2"
       configuration = {
         ProjectName = var.aft_account_customizations_terraform_codebuild_name
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "VENDED_ACCOUNT_ID",
+            value = var.account_id,
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+    ##############################################################
+  # Apply-AFT-Account-VPC
+  ##############################################################
+
+  stage {
+    name = "AFT-Account-Customizations-VPC"
+    action {
+      name            = "Apply"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source-aft-account-customizations-vpc"]
+      version         = "1"
+      run_order       = "2"
+      configuration = {
+        ProjectName = "aft-account-customizations-vpc-terraform"
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "VENDED_ACCOUNT_ID",
+            value = var.account_id,
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+    ##############################################################
+  # Apply-AFT-Account-Customizations-IAM
+  ##############################################################
+
+  stage {
+    name = "AFT-Account-Customizations-IAM"
+    action {
+      name            = "Apply"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source-aft-account-customizations-iam"]
+      version         = "1"
+      run_order       = "2"
+      configuration = {
+        ProjectName = "aft-account-customizations-iam-terraform"
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "VENDED_ACCOUNT_ID",
+            value = var.account_id,
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+  ##############################################################
+  # Apply-AFT-Account-Customizations-SG
+  ##############################################################
+
+  stage {
+    name = "AFT-Account-Customizations-SG"
+    action {
+      name            = "Apply"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source-aft-account-customizations-sg"]
+      version         = "1"
+      run_order       = "2"
+      configuration = {
+        ProjectName = "aft-account-customizations-sg-terraform"
         EnvironmentVariables = jsonencode([
           {
             name  = "VENDED_ACCOUNT_ID",
